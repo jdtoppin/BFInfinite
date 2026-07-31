50b3c30a17a05c8d82279676d248f3bc48da5d2c
jdtoppin <6392799+jdtoppin@users.noreply.github.com>
2026-07-31 16:13:10 -0600

    Defer protected Spellbook styling during combat

--- Modules/Blizzard/Style/PlayerSpellsFrame.lua
+++ Modules/Blizzard/Style/PlayerSpellsFrame.lua
@@ -5,6 +5,37 @@ local S = BFI.modules.Style
 local AF = _G.AbstractFramework
 
 local spellFrame
+local assistedCombatStylePending
+
+local function StyleAssistedCombatFrame(spellBookFrame)
+    if InCombatLockdown() then
+        if assistedCombatStylePending then return end
+        assistedCombatStylePending = true
+        S:RegisterEventOnce("PLAYER_REGEN_ENABLED", function()
+            assistedCombatStylePending = nil
+            StyleAssistedCombatFrame(spellBookFrame)
+        end)
+        return
+    end
+
+    local assistedFrame = spellBookFrame.AssistedCombatRotationSpellFrame
+    S.RemoveTextures(assistedFrame)
+    AF.ClearPoints(assistedFrame)
+    AF.SetPoint(
+        assistedFrame,
+        "BOTTOMRIGHT",
+        spellBookFrame.PagedSpellsFrame,
+        "TOPRIGHT",
+        -10,
+        10
+    )
+
+    local button = assistedFrame.Button
+    S.StyleSpellItemButton(button)
+    button.BFIBackdrop:SetBackdropBorderColor(
+        AF.GetColorRGB("border")
+    )
+end
 
 ---------------------------------------------------------------------
 -- icons
@@ -374,14 +405,7 @@ local function StyleSpellBookFrame()
     end)
 
     -- assisted
-    local assistedFrame = spellBookFrame.AssistedCombatRotationSpellFrame
-    S.RemoveTextures(assistedFrame)
-    AF.ClearPoints(assistedFrame)
-    AF.SetPoint(assistedFrame, "BOTTOMRIGHT", spellBookFrame.PagedSpellsFrame, "TOPRIGHT", -10, 10)
-
-    local button = assistedFrame.Button
-    S.StyleSpellItemButton(button)
-    button.BFIBackdrop:SetBackdropBorderColor(AF.GetColorRGB("border"))
+    StyleAssistedCombatFrame(spellBookFrame)
 
     -- Retail 12.0.7.68887 (Gethe wow-ui-source 4383ced): UpdateVisuals reapplies the active/passive icon masks.
     hooksecurefunc(SpellBookItemMixin, "UpdateVisuals", StyleSpellBookItem)
